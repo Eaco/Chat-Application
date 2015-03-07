ce64713cfbc34de6bb21a88e167c5a045dea3865
Eaco <benjaminmjmorris@gmail.com>
2015-03-07 18:23:13 -0500

    Message permanence
    Whoo! The messages stick around!
    And we can clear them!
    And I'm clearing our the .idea folder, whatever the hell that was

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : process.env.OPENSHIFT_MYSQL_DB_HOST,
+    user     : process.env.OPENSHIFT_MYSQL_DB_USERNAME,
+    password : process.env.OPENSHIFT_MYSQL_DB_PASSWORD,
+    database : 'nodesocketapp',
+    socket   : process.env.OPENSHIFT_MYSQL_DB_SOCKET,
+    port     : process.env.OPENSHIFT_MYSQL_DB_PORT
+});
+connection.connect(function(err, conn) {
+    if(err) {
+        console.log('MySQL connection error: ', err);
+        process.exit(1);
+    }
+
+});
+
 
  app.use(express.static(__dirname + '/public'));
 
@@ -13,17 +30,41 @@ app.get('/', function(req, res){
 
 io.sockets.on('connection', function(socket){
 	console.log('connected!!');
+    connection.query('SELECT Message FROM `Messages`', function(err, msg){
+        for(i = 0; i < msg.length; i++){
+            socket.emit('message', msg[i].Message);
+        }
+    });
 	socket.on('here', function(){
 		socket.broadcast.emit('here');
 	});
 	socket.on('message', function(msg){
 		io.emit('message', msg);
+        sqlMessage(msg);
 	});
     socket.on('clear', function () {
         io.emit('clearing');
+        clearSql();
     })
 });
 
+function sqlMessage(msg) {
+    connection.query('INSERT INTO `Messages`(`Time`, `Message`) VALUES ( NOW(), \'' + msg + '\')', function(err, rows, fields){
+        if(err) throw err;
+    });
+}
+
+function clearSql(){
+    connection.query('DELETE FROM `Messages` WHERE 1', function(err, rows, fields){
+        if(err) throw err;
+    });
+}
+
 http.listen( server_port, server_ip_address, function(){
     console.log('Listening:*');
+    connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields){
+        if(err) throw err;
+
+        console.log(rows[0].solution);
+    });
 });
